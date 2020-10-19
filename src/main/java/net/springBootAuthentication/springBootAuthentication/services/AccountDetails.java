617f2d5b5991bacaa980f19f19a93ed117e12f6e
capstone student <capstonestudent@pn.local>
2020-10-19 09:45:49 +0800

    login with account

--- src/main/java/net/springBootAuthentication/springBootAuthentication/services/AccountDetails.java
+++ src/main/java/net/springBootAuthentication/springBootAuthentication/services/AccountDetails.java
@@ -1,34 +1,44 @@
 package net.springBootAuthentication.springBootAuthentication.services;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import net.springBootAuthentication.springBootAuthentication.model.RegisterModel;
+
 public class AccountDetails implements UserDetails{
+	
+	private static final long serialVersionUID = -7628578506103328683L;
+	private RegisterModel Account;
+	
+	public AccountDetails(RegisterModel account) {
+		Account = account;
+	}
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		// TODO Auto-generated method stub
-		return null;
+		return  new ArrayList<>();
 	}
 
 	@Override
 	public String getPassword() {
 		// TODO Auto-generated method stub
-		return null;
+		return Account.getPassword();
 	}
 
 	@Override
 	public String getUsername() {
 		// TODO Auto-generated method stub
-		return null;
+		return Account.getUsername();
 	}
 
 	@Override
 	public boolean isAccountNonExpired() {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
@@ -40,13 +50,17 @@ public class AccountDetails implements UserDetails{
 	@Override
 	public boolean isCredentialsNonExpired() {
 		// TODO Auto-generated method stub
-		return false;
+		boolean expired = false;
+		if(Account.getExpired().equals("true")) {
+			expired = true;
+		}
+		return expired;
 	}
 
 	@Override
 	public boolean isEnabled() {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 	
 }
