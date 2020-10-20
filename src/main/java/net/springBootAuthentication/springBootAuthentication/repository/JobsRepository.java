d5968755cc050a454916fbbfff1c66fd5656c843
capstone student <capstonestudent@pn.local>
2020-10-20 14:03:08 +0800

    acception

--- src/main/java/net/springBootAuthentication/springBootAuthentication/repository/JobsRepository.java
+++ src/main/java/net/springBootAuthentication/springBootAuthentication/repository/JobsRepository.java
@@ -9,6 +9,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import net.springBootAuthentication.springBootAuthentication.customModel.CustomJobs;
+import net.springBootAuthentication.springBootAuthentication.customModel.CustomUser;
+import net.springBootAuthentication.springBootAuthentication.customModel.Register;
 import net.springBootAuthentication.springBootAuthentication.model.Jobs;
 
 @Repository
@@ -22,10 +24,16 @@ public interface JobsRepository extends JpaRepository<Jobs, Long>{
     public List<String> getFileThroughParameter(@Param("file") String file);
     
     @Query(value = "call getAllJobs(:id)", nativeQuery = true)
-    public List<Jobs> getAllJobs(@Param("id") Long id);
+    public List<CustomJobs> getAllJobs(@Param("id") Long id);
 
     @Query(value = "{call getJobById(:id)}", nativeQuery = true)
     List<CustomJobs> getJobById(@Param("id") Long id);
+
+    @Query(value = "{call getBids(:id)}", nativeQuery = true)
+    List<CustomUser> getBids(@Param("id") Long id);
+
+    @Query(value = "{call getMyJobs(:id)}", nativeQuery = true)
+    List<CustomJobs> getMyJobs(@Param("id") Long id);
 
     
 }
