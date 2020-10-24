089c36cb5edf15b40e2f601bd193d9cde4cbfc8a
capstone student <capstonestudent@pn.local>
2020-10-24 11:13:44 +0800

    initial

--- src/main/java/net/springBootAuthentication/springBootAuthentication/repository/orgMemberRepository.java
+++ src/main/java/net/springBootAuthentication/springBootAuthentication/repository/orgMemberRepository.java
@@ -21,6 +21,9 @@ public interface orgMemberRepository extends JpaRepository<OrgMembers, Long>{
     @Query(value = "{call deleteMemberById(:id)}" , nativeQuery = true)
     void deleteMemberByid(@Param("id") Long id);
 
+    @Query(value = "{call getRoleType()}", nativeQuery = true)
+    Long getRoleType();
+
     @Query(value = "call updateMember(:id, :username, :email, :account_type, :expired, :is_member)", nativeQuery = true)
     void updateMembers(@Param("id") long id, @Param("username") String username, @Param("email") String email, @Param("account_type") String account_type, @Param("expired") Boolean expired, @Param("is_member") Boolean is_member);
 }
