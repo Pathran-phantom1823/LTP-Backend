b812740a2d52fd0a39aece1a306e0329c0a9d168
capstone student <capstonestudent@pn.local>
2020-12-03 22:37:58 +0800

    procedurename

--- src/main/java/net/springBootAuthentication/springBootAuthentication/repository/ForumTransactionsRepository.java
+++ src/main/java/net/springBootAuthentication/springBootAuthentication/repository/ForumTransactionsRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,8 +14,9 @@ import net.springBootAuthentication.springBootAuthentication.customModel.CustomF
 import net.springBootAuthentication.springBootAuthentication.model.ForumTransactionsModel;
 
 public interface ForumTransactionsRepository extends JpaRepository<ForumTransactionsModel, Long>{
-    
-    @Query(value = "{call getPost()}", nativeQuery = true)
+
+    @Transactional
+    @Procedure(procedureName = "getPost")
     public List<CustomForum> getPost();
     
     
